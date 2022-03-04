870fffb7c54dd47897e98d407119400af083ed3d
Dhinesh <76810193+dhinesh256@users.noreply.github.com>
2022-03-04 16:53:51 +0530

    Add files via upload

---
+++ components/ButtonComp.tsx
@@ -0,0 +1,54 @@
+import { View, Text, TouchableOpacity ,Pressable, StyleSheet} from 'react-native'
+import React  from 'react'
+
+type buttonProps = {
+    name : String;
+    navigation? : any ;
+}
+//:React.FC<buttonProps>
+const ButtonComp = (props:buttonProps) => {
+
+    const {name , navigation} = props;
+    return(
+    <View style={styles.container}>
+      <Pressable
+          style={styles.pressable}
+        //onPress={() => navigation.navigate(name)}
+          onPress={()=>alert("clicked")}
+        >
+          <Text style={styles.text}>
+            {`${name}`}
+          </Text>
+
+        </Pressable>
+    </View>
+)}
+
+
+const styles = StyleSheet.create({
+  container : {
+    borderRadius:60,
+    marginLeft:45
+  },
+  pressable:{
+    backgroundColor: "#FFDD00",
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius:40,
+    margin: 5,
+    alignItems:'center',
+    width:"80%",
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '600',
+    color: "#000",
+  },
+
+})
+
+export default ButtonComp;
+
+function alert(arg0: string): void {
+    console.log(arg0);
+}
