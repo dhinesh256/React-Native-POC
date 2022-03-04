b92bf9464f2d47032675cbc66b84f3543531152a
dakshna11 <54468002+dakshna11@users.noreply.github.com>
2022-03-04 16:56:23 +0530

    Add files via upload

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+
+
+const Header: React.FC = () => {
+    return (
+            <View style={styles.headerBar}>
+                <Text style={styles.logo}>\\WU</Text>
+                <Icon name='bars' style={styles.barIcon} />
+            </View>
+    )
+};
+
+export default Header
+
+const styles = StyleSheet.create({
+    headerBar:{
+        flexDirection:"row",
+        backgroundColor:"black",
+        height: "8%",
+        justifyContent: "space-between"
+    },
+    logo:{
+        color: "#ffdd00",
+        fontSize:30,
+        fontWeight:"bold",
+        padding:10,
+        
+    },
+    barIcon:{
+        color: "#ffdd00",
+        fontSize:35,
+        padding:15,
+    },
+    
+})
