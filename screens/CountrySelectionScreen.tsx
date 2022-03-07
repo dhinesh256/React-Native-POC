37325a698a19c232f41091f112d0fc13fce4a610
Dhinesh <76810193+dhinesh256@users.noreply.github.com>
2022-03-07 19:58:02 +0530

    Add files via upload

--- screens/CountrySelectionScreen.tsx
+++ screens/CountrySelectionScreen.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from '../components/Header'
+import CountrySelection from '../components/CountrySelection'
 
 const CountrySelectionScreen = () => {
   return (
     <View>
-      <Text>CountrySelectionScreen</Text>
       <Header/>
+      <CountrySelection/>
     </View>
   )
 }
